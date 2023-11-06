8701b636ca2b2401cb84fe91077b3ab07a712c92
rahulk1705 <kolirahul2405@gmail.com>
2023-11-06 19:42:33 +0530

    Added crypto details

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,26 +7,27 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
-const demoImageUrl = "https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.png"
+const demoImageUrl = "https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.png";
 
 const News = ({ simplified }) => {
-  const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12});
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
   const { data } = useGetCryptosQuery(100);
 
-  if(!cryptoNews?.value) return 'Loading...';
+  if (!cryptoNews?.value) return 'Loading...';
 
   return (
     <Row gutter={[24, 24]}>
+      {/* Adjusted the className to match the CSS */}
       {!simplified && (
-        <Col span={24}>
-          <Select 
+        <Col span={24} className="news-card">
+          <Select
             showSearch
             className="select-news"
             placeholder="Select a Crypto"
             optionFilterProp='children'
-            onChange={(value => setNewsCategory(value))}
-            filterOption={(input, option) => option.children.toLoweCase().indexOf(input.toLowerCase()) >= 0}
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
           >
             <Option value="CryptoCurrency">Cryptocurrency</Option>
             {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
@@ -34,19 +35,19 @@ const News = ({ simplified }) => {
         </Col>
       )}
       {cryptoNews.value.map((news, i) => (
-        <Col xs={24} sm={12} lg={6} key={i}>
-          <Card hoverable className="news-card">
+        <Col xs={24} sm={12} lg={6} key={i} className="news-card">
+          <Card hoverable>
             <a href={news.url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
                 <Title className="news-title" level={4}>{news.name}</Title>
-                <img style={{ maxWidth: '200px', maxHeight: '100px'}} src={news?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news" />
+                <img style={{ maxWidth: '100%', maxHeight: '100%' }} src={news?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news" />
                 <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
                 <div className="provider-container">
                   <div>
                     <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImageUrl} alt="" />
                     <Text className="provider-name">{news.provider[0]?.name}</Text>
                   </div>
-                    <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                  <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                 </div>
               </div>
             </a>
@@ -54,7 +55,7 @@ const News = ({ simplified }) => {
         </Col>
       ))}
     </Row>
-  )
-}
+  );
+};
 
 export default News;
