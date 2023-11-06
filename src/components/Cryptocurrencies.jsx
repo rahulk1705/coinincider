7fad5e648f846b53312944ad9e04782179c03adf
rahulk1705 <kolirahul2405@gmail.com>
2023-11-06 14:17:03 +0530

    Completed crypto news component

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -8,7 +8,7 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 const Cryptocurrencies = ({ simplified }) => {
 
   const count = simplified ? 10 : 100;
-  const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -33,7 +33,7 @@ const Cryptocurrencies = ({ simplified }) => {
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" alt="cryptoImage" src={currency.iconUrl} hoverable />}
+                extra={<img className="crypto-image" alt="cryptoImage" src={currency.iconUrl} />} hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '237e37bfe4mshddc3af16b5f4cf0p15a554jsn0b92190f8361',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
